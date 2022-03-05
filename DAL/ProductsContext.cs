3416b131aa7ce1a99e5b4e90a59fc3b6cfe79b18
Raul Luis <soyrauluis@outlook.com>
2022-03-05 00:51:32 -0400

    Update ProductsContext

--- DAL/ProductsContext.cs
+++ DAL/ProductsContext.cs
@@ -1,32 +1,12 @@
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using Microsoft.EntityFrameworkCore;
 using Blazor.Entidades;
 namespace Blazor.DAL;
 public class ProductsContext : DbContext
 {
-
-    public ProductsContext() { }
-    public ProductsContext(DbContextOptions<ProductsContext> options) : base(options)
-    {
-    }
+    public ProductsContext(DbContextOptions<ProductsContext> options) : base(options) { }
     public DbSet<Productos> Productos { get; set; }
-    public DbSet<ProductoDetalles> ProductoDetalles { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-
-        var configuration = new ConfigurationBuilder()
-                       .SetBasePath(Directory.GetCurrentDirectory())
-                       .AddJsonFile("appsettings.json")
-                       .Build();
-
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
-        optionsBuilder.UseSqlite(connectionString);
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-
-
-    }
 
 }
