4ef754b09a5783fcb002f3644a565404af7a2613
Raul Luis <soyrauluis@outlook.com>
2022-03-05 00:52:12 -0400

    Update models

--- Entidades/ProductoDetalles.cs
+++ Entidades/ProductoDetalles.cs
@@ -1,6 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Blazor.Entidades;
 
 public class ProductoDetalles
@@ -10,8 +8,8 @@ public class ProductoDetalles
     public int ProductoDetallesId { get; set; }
 
     [Required]
-    [MaxLength(50, ErrorMessage = "La descripcion no puede exceder {1} caracteres")]
-    [MinLength(3, ErrorMessage = "La descripcion al menos {1} caracteres")]
+    [MaxLength(50, ErrorMessage = "La presentacion no puede exceder {1} caracteres")]
+    [MinLength(3, ErrorMessage = "La presentacion al menos {1} caracteres")]
     public string? Presentacion { get; set; }
 
     [Required]
@@ -19,22 +17,7 @@ public class ProductoDetalles
     public float? Cantidad { get; set; }
 
     [Required]
-    [Range(0, float.MaxValue, ErrorMessage = "El costo debe ser mayor a {1} y menor a {2}")]
-    public float? Costo { get; set; }
-
-    [Required]
     [Range(0, float.MaxValue, ErrorMessage = "El precio debe ser mayor a {1} y menor a {2}")]
     public float? Precio { get; set; }
-
-    [Required]
-    [Range(0, float.MaxValue, ErrorMessage = "El valor inventario debe ser mayor a {1} y menor a {2}")]
-    public float? ValorInventario { get; set; }
-
-    [Required]
-    [Range(0, 100, ErrorMessage = "La existencia debe ser mayor a {1} y menor a {2}, porque es un %")]
-    public float? Ganancia { get; set; }
-
-    [ForeignKey("ProductoDetallesId")]
-    public Productos? Producto { get; set; }
-
+    public int ProductoId { get; set; }
 }
--- Entidades/Productos.cs
+++ Entidades/Productos.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blazor.Entidades;
 public class Productos
@@ -11,17 +12,27 @@ public class Productos
     [MaxLength(50, ErrorMessage = "La descripcion no puede exceder {1} caracteres")]
     public string? Descripcion { get; set; }
 
-    // [Required]
-    // [Range(0, int.MaxValue, ErrorMessage = "El precio debe ser mayor a {1} y menor a {2}")]
-    // public int Existencia { get; set; }
+    [Required]
+    [Range(0, int.MaxValue, ErrorMessage = "El precio debe ser mayor a {1} y menor a {2}")]
+    public int Existencia { get; set; }
 
-    // [Required]
-    // [Range(0, float.MaxValue, ErrorMessage = "El costo debe ser mayor a {1} y menor a {2}")]
-    // public float Costo { get; set; }
+    [Required]
+    [Range(0, float.MaxValue, ErrorMessage = "El costo debe ser mayor a {1} y menor a {2}")]
+    public float Costo { get; set; }
 
-    // [Required]
-    // [Range(0, float.MaxValue, ErrorMessage = "El costo debe ser mayor a {1} y menor a {2}")]
-    // public float ValorInventario { get; set; }
+    [Required]
+    [Range(0, float.MaxValue, ErrorMessage = "El costo debe ser mayor a {1} y menor a {2}")]
+    public float ValorInventario { get; set; }
 
-    public ICollection<ProductoDetalles>? ProductoDetalles { get; set; }
+    [Required]
+    [Range(1, float.MaxValue, ErrorMessage = "El precio debe ser mayor a {1} y menor a {2}")]
+    public float Precio { get; set; }
+
+    [Required]
+    [Range(0.01, 100, ErrorMessage = "La ganancia debe ser mayor a {1} y menor a {2}")]
+    public float Ganancia { get; set; }
+
+    [ForeignKey("ProductoId")]
+    public virtual List<ProductoDetalles> ProductoDetalles { get; set; } = new List<ProductoDetalles>();
+
 }
